717a3493a7ad35dd3549dce06f15fa69d512d6d3
Futa HIRAKOBA <korosuke613613@gmail.com>
2024-04-27 18:46:09 +0900

    fix

--- src/__tests__/Handler.Callback.test.ts
+++ src/__tests__/Handler.Callback.test.ts
@@ -44,7 +44,7 @@ describe("Test addCallback & execCallback", () => {
           handler.addCallback("CreateIssueWebhook", callbackCustom);
         },
       },
-      expected: { action: "create", type: "Issue", name: "maian" },
+      expected: { action: "create", type: "Issue", name: "main" },
     },
   ];
 
